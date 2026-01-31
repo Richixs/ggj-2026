b33b06d8a5c1ae788cd59394032045731664d9fa
iangameplayhans <iangameplayhans@gmail.com>
2026-01-31 19:24:55 -0400

    feat:funcionalidad de esparcir fuego

--- src/tile_map/Celda/Celda.gd
+++ src/tile_map/Celda/Celda.gd
@@ -1,11 +1,20 @@
 extends Node
 class_name Celda
 
-var coords :Vector2i
-signal chang
-
+var coords :Vector2
+signal change_state_signal(coords)
+signal turn_off_signal(celda)
+@onready var timer = Timer.new()
 # Called when the node enters the scene tree for the first time.
+func change_state():
+	change_state_signal.emit(coords)
+
+func turn_off():
+	turn_off_signal.emit(self)
+
 func _ready() -> void:
+	await get_tree().create_timer(1).timeout.connect(change_state)
+	await get_tree().create_timer(0.5).timeout.connect(turn_off)
 	pass # Replace with function body.
 
 
--- src/tile_map/Terreno.gd
+++ src/tile_map/Terreno.gd
@@ -1,15 +1,26 @@
 extends TileMapLayer
 class_name Terreno
+# corregir que no cambie del estado ceniza a verde al esparcirse
 
+var state_cells = []
+var direction =[Vector2.UP,Vector2.DOWN,Vector2.RIGHT,Vector2.LEFT]
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 
 	pass # Replace with function body.
 
+func turn_off_celda(celda):
+	var cell :Celda = celda
+	change_state_cell(cell.coords)
+	state_cells.erase(celda)
+
 func get_state_cell(position:Vector2):
 	var coords = local_to_map(position)
 	return get_cell_source_id(coords)
-	
+
+func propagar_fuego(position:Vector2):
+	for dir in direction:
+		change_state_cell(position + dir * 64)
 
 func change_state_cell(position: Vector2):
 	var coords = local_to_map(position)
@@ -18,15 +29,24 @@ func change_state_cell(position: Vector2):
 	match state:
 		0:
 			set_cell(coords,1,cell_atlas_coords)
+			var celda = Celda.new()
+			celda.coords = position
+			celda.connect("turn_off_signal",turn_off_celda)
+			celda.connect("change_state_signal",propagar_fuego)
+			add_child(celda)
+			state_cells.append(celda)
 		1:
+			print("estado 1")
 			set_cell(coords,2,cell_atlas_coords)
 		2:
+			print("estado 2")
 			set_cell(coords,0,cell_atlas_coords)
 
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	#print(state_cells)
 	if Input.is_action_just_pressed("Basic Action"):
 		change_state_cell(get_global_mouse_position())
 
