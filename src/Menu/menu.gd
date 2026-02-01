a4f9066e0f9f263389c1e4e121ebe734455744a5
Ricardo Ramirez <ricardo@richixs.dev>
2026-02-01 16:21:23 -0400

    feat: add sounds

--- src/Menu/menu.gd
+++ src/Menu/menu.gd
@@ -2,8 +2,17 @@ extends Control
 
 @export var level :PackedScene
 
+var menu_music = load("res://assets/Audio/menu.ogg")
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	var audio_player = AudioStreamPlayer.new()
+	audio_player.stream = menu_music
+	audio_player.bus = "Musica"
+	add_child(audio_player)
+	audio_player.play()
+	# Ensure looping
+	audio_player.finished.connect(func(): audio_player.play())
 	pass # Replace with function body.
 
 
