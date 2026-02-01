cce940ca4cf96677c80ba115b3393476d2973b62
iangameplayhans <iangameplayhans@gmail.com>
2026-01-31 23:11:10 -0400

    feat:añadir funciones cambiar volumen

--- src/config/Config.gd
+++ src/config/Config.gd
@@ -14,3 +14,23 @@ func _process(delta: float) -> void:
 func _on_return_pressed() -> void:
 	get_tree().call_deferred("change_scene_to_file","res://scenes/menu/Menu.tscn")
 	pass # Replace with function body.
+
+
+func _on_slider_master_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(0,linear_to_db(value))
+	pass # Replace with function body.
+
+
+func _on_slider_music_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(3,linear_to_db(value))
+	pass # Replace with function body.
+
+
+func _on_slider_sfx_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(1,linear_to_db(value))
+	pass # Replace with function body.
+
+
+func _on_slider_cinematica_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(2,linear_to_db(value))
+	pass # Replace with function body.
