76746418905ec9cd2ae29adcbd5e10167175634a
Ricardo Ramirez <ricardo@richixs.dev>
2026-02-01 05:43:57 -0400

    feat: add player animation

--- src/entity/active-entitie/ActiveEntity.gd
+++ src/entity/active-entitie/ActiveEntity.gd
@@ -1,13 +1,9 @@
-extends Entity
+extends Node
 class_name ActiveEntity
 
-@export var speed: float = 200.0
-var input_vector: Vector2 = Vector2.ZERO
+@export var speed := 200.0
+var input_vector := Vector2.ZERO
 
-func _physics_process(delta: float) -> void:
-	if "velocity" in self:
-		self.set("velocity", input_vector * speed)    
-	if self.has_method("move_and_slide"):
-		self.call("move_and_slide")
-	if input_vector.x != 0 and has_node("AnimatedSprite2D"):
-		$AnimatedSprite2D.flip_h = input_vector.x < 0
+func apply_movement(body: CharacterBody2D):
+	body.velocity = input_vector * speed
+	body.move_and_slide()
--- src/entity/active-entitie/entities/Player.gd
+++ src/entity/active-entitie/entities/Player.gd
@@ -1,15 +1,26 @@
-extends ActiveEntity
+extends CharacterBody2D
 class_name Player
 
-func _ready() -> void:
-	var component = get_health_component()
-	if component and component.data:
-		var vida_actual = component.data.current_health
-		print("La vida de " + entity_name + " es: ", vida_actual)
-	else:
-		print("Error: No se encontró el componente o el Resource de datos")
+@onready var entity := $Entity
+@onready var movement := $ActiveEntity
+@onready var anim := $AnimatedSprite2D
 
-func _physics_process(delta: float) -> void:
-	input_vector = Input.get_vector("Left", "Right", "Up", "Down")    
-	super._physics_process(delta)
-	
+func _physics_process(delta):
+	movement.input_vector = Input.get_vector("Left", "Right", "Up", "Down")
+	movement.apply_movement(self)
+	update_animation()
+	update_flip()
+
+func update_animation():
+	if movement.input_vector != Vector2.ZERO:
+		if anim.animation != "run":
+			anim.play("run")
+	else:
+		if anim.animation != "IDLE":
+			anim.play("IDLE")
+
+func update_flip():
+	if movement.input_vector.x < 0:
+		anim.flip_h = false
+	elif movement.input_vector.x > 0:
+		anim.flip_h = true
