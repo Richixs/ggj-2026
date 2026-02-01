e05bf502d6e16e9d1324c6170c7377f27700e6cb
iangameplayhans <iangameplayhans@gmail.com>
2026-02-01 06:09:10 -0400

    feat:add script panel timer

---
+++ src/gui/panel_timer.gd
@@ -0,0 +1,15 @@
+extends Panel
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Timer.start(90)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var time:=Time.get_offset_string_from_offset_minutes($Timer.time_left)
+	time = time.substr(1)
+	$Label.text = time
+	pass
