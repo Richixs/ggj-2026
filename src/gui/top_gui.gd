5a28d2fca0d2fbaf55fa9293361a554261c98c82
iangameplayhans <iangameplayhans@gmail.com>
2026-02-01 09:20:34 -0400

    feat: gui func cambiar pantalla a derrota

--- src/gui/top_gui.gd
+++ src/gui/top_gui.gd
@@ -19,6 +19,8 @@ func _ready() -> void:
 			textures[1].visible = false
 		elif hp_player <= 34:
 			textures[2].visible = false
+			get_tree().call_deferred("change_scene_to_file","res://scenes/pantallas_finales/derrota/derrota.tscn")
+
 		else:
 			print("Perdiste")
 	pass # Replace with function body.
--- src/pantallas_finales/derrota/derrota.gd
+++ src/pantallas_finales/derrota/derrota.gd
@@ -12,4 +12,5 @@ func _process(delta: float) -> void:
 
 
 func _on_button_pressed() -> void:
+	get_tree().call_deferred("change_scene_to_file","res://scenes/Main.tscn")
 	pass # Replace with function body.
