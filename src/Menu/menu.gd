2ec3553b5c30d9cee76b7ca43f01af0c0e209a24
iangameplayhans <iangameplayhans@gmail.com>
2026-01-31 21:58:20 -0400

    feat:func cambiar escena a nivel

--- src/Menu/menu.gd
+++ src/Menu/menu.gd
@@ -1,5 +1,6 @@
 extends Control
 
+@export var level :PackedScene
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +10,8 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_button_pressed() -> void:
+	get_tree().call_deferred("change_scene_to_packed",level)
+	pass # Replace with function body.
