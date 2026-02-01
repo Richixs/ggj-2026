994167a0df547743f8d9e1822d941d8081759f58
iangameplayhans <iangameplayhans@gmail.com>
2026-02-01 02:30:38 -0400

    feat:func distance between tree add

--- src/levels/level_1.gd
+++ src/levels/level_1.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
-@export var cantidad_arboles :int = 5
+@export var cantidad_arboles :int = 8
 @export var distance_borrow : int = 500
 
+var arboles :Array[Node2D]= []
 var random := RandomNumberGenerator.new()
 var tree = preload("res://scenes/passive-entities/tree/tree.tscn")
 
@@ -10,6 +11,7 @@ func create_tree(position:Vector2):
 	var tree_instance :Node2D= tree.instantiate()
 	tree_instance.global_position = position
 	add_child(tree_instance)
+	arboles.append(tree_instance)
 
 
 # Called when the node enters the scene tree for the first time.
@@ -19,8 +21,17 @@ func _ready() -> void:
 	while i < cantidad_arboles:
 		var pos = Vector2(random.randf_range(-1600,1600),random.randf_range(-930,930))
 		if pos.distance_to(Vector2.ZERO) >= distance_borrow:
-			i +=1
-			create_tree(pos)
+			if not arboles.is_empty():
+				var can_create_tree = true
+				for tree in arboles:
+					if pos.distance_to(tree.global_position) < 500:
+						can_create_tree = false
+				if can_create_tree:
+					i +=1
+					create_tree(pos)
+			else:
+				i += 1
+				create_tree(pos)
 	pass # Replace with function body.
 
 
